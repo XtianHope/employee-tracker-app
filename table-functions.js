f01b715f491e4a4681bb7a9ec47df99501e419d6
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-28 18:00:40 -0500

    added a function to display data - will be used for additional functions

--- table-functions.js
+++ table-functions.js
@@ -2,6 +2,12 @@
 const inquirer = require('inquirer');
 const connection = require('./db/connection.js');
 
+// Function to display data
+function displayData(data) {
+    console.table(data);
+    startApp();
+}
+
 // Function to start the app
 // Defining inquirer prompt & logic for each action
 function startApp() {
