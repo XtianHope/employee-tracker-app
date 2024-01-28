77f183cf710c1538802027d89161e5b45e4e4ca0
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-28 15:34:02 -0500

    added function to start app & definied the inquirer prompt & logic for each action selected

--- db/index.js
+++ db/index.js
@@ -1,3 +1,46 @@
 // Import inquirer & connection
 const inquirer = require('inquirer');
 const connection = require('./db/connection.js');
+
+// Function to start the app
+// Defining inquirer prompt & logic for each action
+function startApp() {
+    inquirer
+    .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'Choose an action:',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update Employee Role', 'Exit']
+    })
+    .then((answer) => {
+        switch (answer.action) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;    
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add A Department':
+                addDepartment();
+                break;
+            case 'Add A Role':
+                addRole();
+                break;
+            case 'Add An Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
