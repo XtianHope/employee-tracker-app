fc196c039314995fae927edf649c98905432b3b5
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-28 15:58:14 -0500

    to create table for employee - added place hold messages

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,10 @@ CREATE TABLE role (
 );
 
 -- Create table for Employees
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30), --To hold employee first name
+    last_name VARCHAR(30), --To hold employee last name
+    role_id INT, --To hold reference to role employee has
+    manager_id INT --To hold reference to another employee that is manager of the current employee. This field may be null if employee has no manager.
+);
