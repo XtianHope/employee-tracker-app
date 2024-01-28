d78e61baabccbc59dd072fb7ee2844af0296aa39
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-28 15:45:21 -0500

    added the connection to mysql database, but left notes for info that i think needs to be changed

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,10 @@
 // Connection to SQL
 const mysql = require('mysql2');
+
+// Connect to MySQL database
+const connection = mysql.createConnection({
+    host: 'localhost', // NOTE: Need to add MySQL host
+    user: 'root', // NOTE: Need to add MySQL username
+    password: 'password', // NOTE: Need to add MySQL password
+    database: 'employee_tracker_db' // NOTE: Need to add MySQL database name
+});
