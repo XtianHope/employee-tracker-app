0ab36906b7b824f9fe4ac5978f6653bd5dc4a9d1
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-28 23:21:11 -0500

    updated my schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,32 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
 -- Create Table for Departments
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) --To hold department name
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL -- To hold department name
 );
 
--- Create talbe for Roles
+-- Create table for Roles
 CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30), --To hold role title
-    salary DECIMAL, --To hold role salary
-    department_id INT --To hold reference to department role belongs to
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, -- To hold role title
+    salary DECIMAL, -- To hold role salary
+    department_id INTEGER, -- To hold reference to department role belongs to
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 -- Create table for Employees
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30), --To hold employee first name
-    last_name VARCHAR(30), --To hold employee last name
-    role_id INT, --To hold reference to role employee has
-    manager_id INT --To hold reference to another employee that is manager of the current employee. This field may be null if employee has no manager.
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30), -- To hold employee first name
+    last_name VARCHAR(30), -- To hold employee last name
+    role_id INTEGER, -- To hold reference to role employee has
+    manager_id INTEGER, -- To hold reference to another employee that is the manager of the current employee. This field may be null if the employee has no manager.
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
