f9d3aece342e9729eb341297bff6a8db6dfef8ff
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2024-01-29 22:18:53 -0500

    made changes due to errors with mysql, pushing all changes

--- db/schema.sql
+++ db/schema.sql
@@ -8,25 +8,25 @@ DROP TABLE IF EXISTS employee;
 
 -- Create Table for Departments
 CREATE TABLE department (
-    id INTEGER auto-increment PRIMARY KEY,
-    name VARCHAR(30) NOT NULL -- To hold department name
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 -- Create table for Roles
 CREATE TABLE role (
-    id INTEGER auto-increment PRIMARY KEY,
-    title VARCHAR(30) NOT NULL, -- To hold role title
-    salary DECIMAL, -- To hold role salary
-    department_id INTEGER, -- To hold reference to department role belongs to
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER, 
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 -- Create table for Employees
 CREATE TABLE employee (
-    id INTEGER auto-increment PRIMARY KEY,
-    first_name VARCHAR(30), -- To hold employee first name
-    last_name VARCHAR(30), -- To hold employee last name
-    role_id INTEGER, -- To hold reference to role employee has
-    manager_id INTEGER, -- To hold reference to another employee that is the manager of the current employee. This field may be null if the employee has no manager.
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30), 
+    role_id INTEGER, 
+    manager_id INTEGER, 
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 -- Insert data into Department Table --ID is auto-incremented
-INSERT INTO department (id, name) VALUES
+INSERT INTO department (name) VALUES
 ('Sales'),
 ('Engineering'),
 ('Finance'),
 ('Legal');
 
 -- Insert data into Role Table --ID is auto-incremented
-INSERT INTO role (id, title, salary, department_id) VALUES
+INSERT INTO role (title, salary, department_id) VALUES
 ('Sales Lead', 100000, 1),
 ('Salesperson', 80000, 1),
 ('Lead Engineer', 150000, 2),
@@ -17,14 +17,14 @@ INSERT INTO role (id, title, salary, department_id) VALUES
 ('Lawyer', 190000, 4);
 
 -- Insert data into Employee Table --ID is auto-incremented
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
-('Emily', 'Davis', 1, NULL), --Emily is a Sales Lead -- Emily is the manager of Isabella 
-('Isabella', 'White', 2, 1), --Isabella is a Salesperson -- Isabella reports to Emily
-('Ethan', 'Martinez', 3, NULL), --Ethan is a Lead Engineer -- Ethan is the manager of James 
-('Liam', 'Parker', 4, 3), --Liam is a Software Engineer -- Liam reports to Ethan 
-('Aria', 'Murphy', 5, NULL), --Aria is an Account Manager -- Aria is the manager of Michael 
-('Michael', 'Carter', 6, 5), --Michael is an Accountant -- Michael reports to Aria 
-('Natalie', 'Foster', 7, NULL), --Natalie is a Legal Team Lead -- Natalie is the manager of James 
-('James', 'Thompson', 8, 7), --James is a Lawyer -- James reports to Natalie 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+('Emily', 'Davis', 1, NULL),
+('Isabella', 'White', 2, 1),
+('Ethan', 'Martinez', 3, NULL),
+('Liam', 'Parker', 4, 3),
+('Aria', 'Murphy', 5, NULL),
+('Michael', 'Carter', 6, 5),
+('Natalie', 'Foster', 7, NULL),
+('James', 'Thompson', 8, 7);
 
 
